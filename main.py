fb5a021379c55956a78a071ff3a4ed8dac54634a
otronity <92176228+otronity@users.noreply.github.com>
2024-09-29 18:06:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+def total_salary(path):
+    total = 0
+    count = 0
+    average = 0
+
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                name, salary = line.strip().split(',')
+                total += int(salary)
+                count += 1
+
+        if count == 0:
+            average = 0
+        else:
+            average = total / count
+            
+    except FileNotFoundError:
+        print(f"Файл за вказаним шляхом '{path}' не знайдено.")
+    except (IOError, OSError) as e:
+        print(f"Ошибка чтения файла, возможно он повержден: {e}")
+    except ValueError as ve:
+        print(f"Помилка при обробці даних: {ve}")
+    finally:
+        return total, average
+    
+
+total, average = total_salary("salaries.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
